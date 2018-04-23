6dfb01806d35174c49bf723091a17f00eb0b04c4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ChartLesson2/Controllers/ChartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevExpress.Web.Mvc;
+
+namespace ChartLesson2.Controllers {
+    public class ChartController : Controller {
+        public ActionResult Index() {
+            using (var dbContext = new Models.NwindDbContext()) {
+                var category =
+                    (from c in dbContext.Categories
+                     where c.CategoryName == "Beverages"
+                     select c).First();
+                var query = 
+                    from p in dbContext.Products
+                    where p.CategoryID == category.CategoryID
+                    select p;
+                return View(query.ToList());
+            }
+        }
+    }
+}
---
+++ VB/ChartLesson2/Controllers/ChartController.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports DevExpress.Web.Mvc
+
+Namespace ChartLesson2.Controllers
+    Public Class ChartController
+        Inherits Controller
+
+        Public Function Index() As ActionResult
+            Using dbContext = New Models.NwindDbContext()
+                Dim category = ( _
+                    From c In dbContext.Categories _
+                    Where c.CategoryName = "Beverages" _
+                    Select c).First()
+                Dim query = From p In dbContext.Products _
+                            Where p.CategoryID = category.CategoryID _
+                            Select p
+                Return View(query.ToList())
+            End Using
+        End Function
+    End Class
+End Namespace
